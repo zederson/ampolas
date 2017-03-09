ac83cc4984f0c51cf20a4873c2fbd220d5348179
Ederson de Lima <edersondelima@gmail.com>
2017-03-08 22:25:37 -0300

    added devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session,
+                       if:   proc { |c| c.request.format == 'application/json' }
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,24 @@ Bundler.require(*Rails.groups)
 module Ampolas
   class Application < Rails::Application
 
-    config.autoload_paths += %W(#{config.root}/lib )
-    config.log_tags = [:uuid]
+    config.eager_load_paths += %W(#{config.root}/lib)
+    config.log_tags ||= []
+    config.log_tags += [:uuid]
+
+    config.i18n.default_locale = 'pt-BR'
+    config.i18n.enforce_available_locales = true
     config.encoding = 'utf-8'
     config.time_zone = 'Brasilia'
-    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false
 
+    config.generators do |generate|
+      generate.test_framework :rspec
+      generate.stylesheets false
+      generate.javascript_engine false
+      generate.view_specs false
+      generate.helper false
+      generate.factory_girl dir: 'spec/factories'
+    end
   end
 end
